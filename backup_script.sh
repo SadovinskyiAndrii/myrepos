af08dabcfcfe11021d6b9c0de673919496c23fa4
HolinkoYaroslav <tagasik@ukr.net>
2024-06-20 18:48:36 +0300

    add script

---
+++ backup_script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Шлях до директорії, яку потрібно резервувати
+SOURCE_DIR="/mnt/d/home "
+
+# Шлях до директорії для зберігання резервних копій
+BACKUP_DIR="/mnt/d "
+
+# Поточна дата та час
+DATE_PART=$(date +%Y%m%d)
+TIME_PART=$(date +%H%M)
+DATE=$(printf "%s_%s" "$DATE_PART" "$TIME_PART")
+
+# Ім'я файлу резервної копії
+BACKUP_FILE="backup_$DATE.tar.gz"
+
+# Створення резервної копії
+tar -czf "$BACKUP_DIR/$BACKUP_FILE" "$SOURCE_DIR"
